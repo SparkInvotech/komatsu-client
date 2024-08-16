b20c9ffa17ea90eb53a151b32fad088bdd20164e
PRAVEEN S <130171193+Pandu-Praveen@users.noreply.github.com>
2024-08-16 23:57:13 +0530

    final commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,12 @@
-import path from "path";
-import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { resolve } from "path";
 
-const basenameProd = "/shadcn-sample";
-
-export default defineConfig(({ command }) => {
-  const isProd = command === "build";
-
-  return {
-    base: isProd ? basenameProd : "",
-    plugins: [react()],
-    resolve: {
-      alias: {
-        "@": path.resolve(__dirname, "./src"),
-      },
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "src"),
     },
-    define: {
-      global: {
-        basename: isProd ? basenameProd : "",
-      },
-    },
-  };
+  },
 });
